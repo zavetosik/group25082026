77278666390e08c7e66eaca9621e2918c8a43e7c
draxc <sasacikota97@gmail.com>
2026-09-08 11:21:12 +0300

    +dz_2

--- app/project/utils.py
+++ app/project/utils.py
@@ -7,3 +7,15 @@ def is_number_positive(number: int | float) -> bool:
     result = number > 0
     # print('look here', number, result)
     return result
+
+
+def is_password_safe(password: str) -> bool:
+    has_8letters = len(password) >= 8
+    has_digit = any(char.isdigit() for char in password)
+    has_letter = any(char.isalpha() for char in password)
+    has_symbols = any(not char.isalnum() for char in password)
+    has_space = any(char.isspace() for char in password)
+    if has_8letters and has_digit and has_letter and has_symbols and not has_space:
+        return True
+    else:
+        return False
