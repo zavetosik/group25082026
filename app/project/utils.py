1c2c93842cc134ba1e7cd84a9bd37134d2d5190b
zavetosik <sasacikota97@gmail.com>
2026-09-10 17:38:08 +0300

    +dz_2 (#5)

--- app/project/utils.py
+++ app/project/utils.py
@@ -9,6 +9,18 @@ def is_number_positive(number: int | float) -> bool:
     return result
 
 
+
+def is_password_safe(password: str) -> bool:
+    has_8letters = len(password) >= 8
+    has_digit = any(char.isdigit() for char in password)
+    has_letter = any(char.isalpha() for char in password)
+    has_symbols = any(not char.isalnum() for char in password)
+    has_space = any(char.isspace() for char in password)
+    if has_8letters and has_digit and has_letter and has_symbols and not has_space:
+        return True
+    else:
+        return False
+
 def calculate_discount(price: int | float, discount: int | float) -> int | float:
     return price * (1 - discount / 100)
 
@@ -18,3 +30,4 @@ def is_even(number: int) -> bool:
 
 def get_full_name(first_name: str, last_name: str) -> str:
     return f'{first_name} {last_name}'
+
