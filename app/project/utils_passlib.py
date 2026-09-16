d4e6edd997d80bef4c8a5c917bf057c47e865e43
draxc <sasacikota97@gmail.com>
2026-09-16 13:49:22 +0300

    dz_7

--- app/project/utils_passlib.py
+++ app/project/utils_passlib.py
@@ -2,7 +2,10 @@ from passlib.context import CryptContext
 
 context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-password = "aa"
+password = "aa35ui"
 hash = context.hash(secret=password)
 print(hash)
+
+is_valid = context.verify(secret=password, hash=hash)
+print(is_valid)
 
