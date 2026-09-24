b0616c6ddfa42dd8905ed41cea8c161139ec9867
draxc <sasacikota97@gmail.com>
2026-09-24 19:21:16 +0300

    dz_9

--- app/project/publisher.py
+++ app/project/publisher.py
@@ -1,1 +1,7 @@
-from redis_utils import publisher
+from redis_utils.redis_client import redis_client
+
+
+for i in range(10):
+    message = f"Hello Redis! Message #{i}"
+    redis_client.publish("python_channel", message)
+    print(message)
--- app/project/subscriber.py
+++ app/project/subscriber.py
@@ -1,1 +1,8 @@
-from redis_utils import consumer
+from redis_utils.redis_client import redis_client
+
+subscriber = redis_client.pubsub()
+subscriber.subscribe("python_channel")
+
+for message in subscriber.listen():
+    if message["type"] == "message":
+        print(message["data"])
